ff0126c1573d672e3777c32679f3d3424f5b192a
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2025-06-03 20:21:39 -0300

    Subindo o html com o js funcionando

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
 function converterCtoF(tempC) {
-    const tempF = (tempC * 9/5) + 32
+    const tempF = (tempC * 9/5) + 32;
     console.log(`Temperatura = ${tempF} F`);
-}
-
-function convertCtoK(tempC) {
-    
+    /* o return retorna o que é desejado, neste caso a temp convertida que está na variável tempF */
+    return tempF;
 }
 
 function converter() {
     console.log('Ei Clicou!'); 
+    let tempC = document.getElementById('tempC').value;
+    console.log(`Temp °C: ${tempC}`);   
+
+    /* Chamando a função que converte de C para F */
+    const resultado = converterCtoF(tempC);
+    /* Exibe o reultado na tag que possui um id chamado resultado no HTML */
+    document.getElementById('resultado').innerHTML = resultado;
 }
 
-converterCtoF(18)
-converterCtoF(30)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* converterCtoF(18)
+converterCtoF(30) */
+
+
+/* function convertCtoK(tempC) {
+    
+} */
